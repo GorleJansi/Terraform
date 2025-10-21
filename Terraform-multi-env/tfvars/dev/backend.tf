0ad006fff6ab46f6860063bdf56a0c9806f59bf7
GorleJansi <gorlejansi2001@gmail.com>
2025-10-21 22:19:56 +0530

    project

--- Terraform-multi-env/tfvars/dev/backend.tf
+++ Terraform-multi-env/tfvars/dev/backend.tf
@@ -1,5 +1,5 @@
-    bucket        = "roboshop1-remotestate"   # Name of the S3 bucket where state file will be stored
-    key           = "tfvars-multi-env-demo"   # Path inside the S3 bucket for this specific Terraform state
-    region        = "us-east-1"               # AWS region where the S3 bucket exists
-    use_lockfile  = true                      # Enables state locking to prevent concurrent modifications (Prevents two people from modifying state simultaneously.)
-    encrypt       = true                      # Enable server-side encryption for the state file in S3
+    bucket        = "roboshop1-remotestate-dev"   # Name of the S3 bucket where state file will be stored diff for each env
+    key           = "tfvars-multi-env-demo"       # Path inside the S3 bucket for this specific Terraform state
+    region        = "us-east-1"                   # AWS region where the S3 bucket exists
+    use_lockfile  = true                          # Enables state locking to prevent concurrent modifications (Prevents two people from modifying state simultaneously.)
+    encrypt       = true                          # Enable server-side encryption for the state file in S3
