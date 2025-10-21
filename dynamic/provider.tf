13b7b71320a27acc92b56f62cad42b8c95e29760
GorleJansi <gorlejansi2001@gmail.com>
2025-10-21 15:24:49 +0530

    project

--- dynamic/provider.tf
+++ dynamic/provider.tf
@@ -1,17 +1,14 @@
 # Define terraform {} block → Main Terraform configuration block. Defines required providers and backend settings.
 
 terraform {
-  
-  # Specify the providers required for this Terraform configuration
-  required_providers {
+  required_providers {           # Specify the providers required for this Terraform configuration
     aws = {                      # The provider is AWS
       source  = "hashicorp/aws"  # Source location of the provider (official HashiCorp registry)
       version = "6.16.0"         # Version of the AWS provider to use
     }
   }
 
-  # Configure the backend to store Terraform state remotely
-  backend "s3" {
+  backend "s3" {                              # Configure the backend to store Terraform state remotely
     bucket        = "roboshop1-remotestate"   # Name of the S3 bucket where state file will be stored
     key           = "dynamic-block-demo"      # Path inside the S3 bucket for this specific Terraform state
     region        = "us-east-1"               # AWS region where the S3 bucket exists
@@ -20,7 +17,6 @@ terraform {
   }
 }
 
-# Configure the AWS provider with region details
-provider "aws" {
-  region = "us-east-1"  # Set the default AWS region for all resources managed by this provider
+provider "aws" {                        # Configure the AWS provider with region details
+  region = "us-east-1"                  # Set the default AWS region for all resources managed by this provider
 }
--- dynamic/sg.tf
+++ dynamic/sg.tf
@@ -1,22 +1,19 @@
-# Define a new AWS Security Group resource called "roboshop_instance"
 
-resource "aws_security_group" "roboshop_instance" {
-  
-  name   = "roboshop-strict-sg"                   # Name of the security group in AWS
+resource "aws_security_group" "roboshop_instance" {    # Define a new AWS Security Group resource called "roboshop_instance"
+  name   = "roboshop-strict-sg"                        # Name of the security group in AWS
 
   egress {                                        # Define outbound traffic rules (egress)
-    from_port   = 0                               # Start port 0
-    to_port     = 0                               # End port 0
+    from_port   = 0                               # Start port in the range 0
+    to_port     = 0                               # End port in the range 0 if from_port=to_port only that one specific port is allowed.
     protocol    = "-1"                            # -1 means all protocols (TCP, UDP, ICMP etc.)
     cidr_blocks = ["0.0.0.0/0"]                   # Allow traffic to all IPs on the internet
   }
   
-  # Define inbound traffic rules (ingress) dynamically based on a variable
-  dynamic "ingress" {
-    for_each = toset(var.ingress_ports)  # Loop through each port in the "ingress_ports" variable
+  dynamic "ingress" {                           # Define inbound-traffic-rules(ingress) dynamically based on a variable
+    for_each = toset(var.ingress_ports)         # Loop through each port in the "ingress_ports" variable
     content {
-        from_port   = ingress.value       # The port number from the loop (inbound start port)
-        to_port     = ingress.value       # Same port as end port (single port rule)
+        from_port   = ingress.value       # reference the current item using BLOCK_LABEL.value.(inbound start port)
+        to_port     = ingress.value       # reference the current item using BLOCK_LABEL.value.
         protocol    = "tcp"               # Only allow TCP protocol
         cidr_blocks = ["0.0.0.0/0"]      # Allow traffic from all IPs on the internet
     }
