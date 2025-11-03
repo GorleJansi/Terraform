5ba1e682d43443418d40c27bc9aa30fbf9b081ee
GorleJansi <gorlejansi2001@gmail.com>
2025-11-03 15:04:38 +0530

    project

--- terraform-vpc-Test-module/main.tf
+++ terraform-vpc-Test-module/main.tf
@@ -1,23 +1,24 @@
 module "vpc" {
-    #source = "../terraform-aws-vpc"
+      #source = "../terraform-aws-vpc"
     source = "git::https://github.com/GorleJansi/Terraform.git//terraform-vpc-module?ref=main"  # git:: → tells Terraform to pull the module from a Git repository.
-    # vpc_cidr = "10.0.0.0/16"
-    # project_name = "roboshop"
-    # environment = "dev"
+    # manual values    
+      # vpc_cidr = "10.0.0.0/16"    
+      # project_name = "roboshop"
+      # environment = "dev"
 
-    # VPC
+# VPC
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
     vpc_tags = var.vpc_tags
 
-    # public subnets
+# public subnets
     public_subnet_cidrs = var.public_subnet_cidrs
 
-    # private subnets
+# private subnets
     private_subnet_cidrs = var.private_subnet_cidrs
 
-    # database subnets
+# database subnets
     database_subnet_cidrs = var.database_subnet_cidrs
 
     is_peering_required = true
